219f48b0c4fe33f63aa7af002ffbc3e07e75590a
Isaac Dexter <d015349l@student.staffs.ac.uk>
2022-11-15 12:33:42 +0000

    Set out function prototypes for server

--- ServerProj/Program.cs
+++ ServerProj/Program.cs
@@ -2,10 +2,45 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
 
 namespace ServerProj
 {
+    public class Server
+    {
+        private TcpListener tcpListener;
+
+        public Server(string ipAddress, int port)
+        {
+
+        }
+
+        public void Start()
+        {
+
+        }
+
+        public void Stop()
+        {
+
+        }
+
+        private void ClientMethod(Socket socket)
+        {
+
+        }
+
+        private string GetReturnMessage(string code)
+        {
+            return "";
+        }
+    }
+
+
+
     internal class Program
     {
         static void Main(string[] args)
