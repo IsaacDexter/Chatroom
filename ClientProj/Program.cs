ac66dc09955cb0cbb49eacf8b44fddc41b96d7ce
Isaac Dexter <d015349l@student.staffs.ac.uk>
2022-11-16 11:16:47 +0000

    Set out function prototypes for the client according to the following UML
    -  tcpClient: TcpClient
    -  stream: NetworkStream
    -  writer: StreamWriter
    -  reader: StreamReader
    + Client()
    + Connect(ipAddress: string, port: int) : bool
    + Run()
    -  ProcessServerResponse()

--- ClientProj/Program.cs
+++ ClientProj/Program.cs
@@ -2,10 +2,40 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
 
 namespace ClientProj
 {
+    public class Client
+    {
+        private TcpClient tcpClient;
+        private NetworkStream stream;
+        private StreamWriter writer;
+        private StreamReader reader;
+        public Client()
+        {
+
+        }
+
+        public bool Connect(IPAddress ipAddress, int port)
+        {
+            return true;
+        }
+
+        public void Run()
+        {
+
+        }
+
+        private void ProcessServerResponse()
+        {
+
+        }
+    }
     internal class Program
     {
         static void Main()
